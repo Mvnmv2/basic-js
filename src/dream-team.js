8aabfe2d908aa174604b8c9452a418ed9b8556c2
Andrey <maven23@rambler.ru>
2023-04-12 19:09:55 +0300

    init baseJS

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if(!Array.isArray(members)) return false
+  let code = ''
+
+  code = members.map( item => {
+    if ( typeof(item) === 'string' ) {
+      return item.trim()[0].toUpperCase() }
+  }).filter(item => item).sort().join('')
+
+  return code
 }
 
 module.exports = {
